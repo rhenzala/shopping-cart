40f0e5719633796e4ae82673928e22321f51019e
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-26 11:43:13 +0800

    Install react-router and lucide-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#f8fafc',
+      'black': '#020617',
+      'red': '#dc2626',
+    },
     extend: {},
   },
   plugins: [],
