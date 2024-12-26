73ac95536e11752e3b0aa65976631603706baf8d
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-26 19:46:40 +0800

    Remove header/*, put header content in App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './components/App.jsx'
+import HomePage from './components/main/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "home",
+    element: <HomePage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
