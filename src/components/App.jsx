dd16264cbe57d287b38204995684d5426a8a3eca
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-26 17:44:03 +0800

    Add main content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 
 import '../App.css'
 import Header from './header/Header'
+import MainContent from './main/MainContent'
 
 function App() {
 
   return (
     <>
       <Header />
+      <MainContent />
     </>
   )
 }
