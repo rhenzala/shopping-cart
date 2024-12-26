de1d5a68189638baf51031d79aa961bef5f1cd18
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-26 11:41:44 +0800

    Add header content and style it

--- src/components/header/HamburgerMenu.jsx
+++ src/components/header/HamburgerMenu.jsx
@@ -1,20 +1,20 @@
 
 
-const HamburgerMenu = ({ menuItems, handleMenuClick, isOpen }) => {
+const HamburgerMenu = ({ handleMenuClick, isOpen }) => {
     return (
-        <div>
             <button
             onClick={() => handleMenuClick()}
+            className="md:hidden p-2 rounded-md ml-auto"
             aria-expanded={isOpen}
             aria-label="Main menu"
             >
-                <div>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <div className="space-y-2">
+                    <span className={`block w-6 h-0.5 bg-white transform transition duration-300 delay-150 ${isOpen ? 'rotate-45 translate-y-2.5' : ''}`}></span>
+                    <span className={`block w-6 h-0.5 bg-white transform transition duration-300 delay-150 ${isOpen ? 'opacity-0' : ''}`}></span>
+                    <span className={`block w-6 h-0.5 bg-white transform transition duration-300 delay-150 ${isOpen ? '-rotate-45 -translate-y-2.5' : ''}`}></span>
                 </div>
             </button>
-        </div>
+            
     )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,80 @@
 import { useState } from "react"
 import renshop from '../../assets/logo/renshop.png'
 import HamburgerMenu from "./HamburgerMenu"
+import { Search, ShoppingCart } from 'lucide-react'
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
 
     const menuItems = [
         {label: 'Home', href: '#'},
-        {label: 'Home', href: '#'},
+        {label: 'Shop', href: '#'},
+        {label: 'About', href: '#'},
     ]
 
     const handleMenuClick = () => {
         setIsOpen(!isOpen)
     }
     return (
-        <header>
-            <div className="logo">
-                <img src={renshop} alt="Renshop logo" />
-            </div>
-            <div className="navBar">
-            <nav>
-            {menuItems.map(item =>(
-                <a key={item.label}
-                href={item.href}>
-                    {item.label}
-                </a>
-            ))}
-            </nav>
+        <header className="bg-red w-full md:relative fixed sm:fixed">
+            <div className="w-full px-4">
+                <div className="flex items-center justify-between">
+                    <div className="logo">
+                        <img 
+                        src={renshop}
+                        className="h-16" 
+                        alt="Renshop logo" />
+                    </div>
+                    <div className="flex gap-4 items-center">
+                        <div className="hidden sm:hidden md:flex space-x-8 ml-auto">
+                            <nav>
+                            {menuItems.map(item =>(
+                                <a key={item.label}
+                                href={item.href}
+                                className="text-white hover:underline-offset-8 px-3 py-2"
+                                >
+                                    {item.label}
+                                </a>
+                            ))}
+                            </nav>
+                        </div>
+                        <div className="flex gap-4">
+                            <div className="md:flex gap-1 hidden sm:hidden">
+                                <input 
+                                type="search"
+                                placeholder="Search" 
+                                className="px-2 rounded-md"
+                                />
+                                <button>
+                                    <Search color="#020617" size={24}/>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <ShoppingCart color="#020617" size={24}/>
+                                </button>
+                            </div>
+                        </div>
+                        <HamburgerMenu
+                        handleMenuClick={handleMenuClick}
+                        isOpen={isOpen}
+                        />
+                    </div>
+                </div>
+                <div className={`bg-red md:hidden absolute top-16 left-0 right-0 bg-gray-800 transform transition-all duration-300 delay-150 ease-in-out origin-top ${
+              isOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0'
+            }`}> 
+                    <nav  className="px-2 pt-2 pb-3 space-y-1 flex flex-col">
+                    {menuItems.map((item) =>(
+                        <a key={item.label}
+                        href={item.href}
+                        className='block text-white px-3 py-2 rounded-md'
+                        >
+                            {item.label}
+                        </a>
+                    ))}
+                    </nav>
+                </div>
             </div>
         </header>
     )
