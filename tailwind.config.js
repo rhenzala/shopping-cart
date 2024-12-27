b9d4a0bff157bf2024d920d2be194a991d45af15
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-27 15:39:23 +0800

    Add text-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,24 @@ export default {
       screens: {
         "custom-md": { max: "768px" },
       },
+      textShadow: {
+        'default': '0 2px 5px rgba(0, 0, 0, 0.2)',
+        'lg': '0 2px 10px rgba(0, 0, 0, 0.5)',
+        'xl': '0 2px 15px rgba(0, 0, 0, 0.7)',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    },
+  ],
 }
 
